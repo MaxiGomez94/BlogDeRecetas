052120d16f63cfa2f9440c09ac38e6e2069d68c5
Maximiliano Gomez <alexander34223@gmail.com>
2024-02-28 12:39:49 -0300

    me olvide de guarda la funcion de intenRecipes

--- src/components/pages/recipes/ItemRecipes.jsx
+++ src/components/pages/recipes/ItemRecipes.jsx
@@ -2,8 +2,12 @@ import { Button } from "react-bootstrap";
 import Swal from "sweetalert2";
 import { borrarRecetaAPI, mostraRecetaAPI } from "../../../helpers/queris";
 import { Link } from "react-router-dom";
-const ItemRecipes = ({receta,funcionSetReceta}) => {
-  const borrarRecetas= ()=>{
+
+
+
+const ItemRecipes = ({ receta, funcionSetReceta }) => {
+  
+  const borrarRecetas = () => {
     Swal.fire({
       title: "Estas Seguro de Eliminar la Receta?",
       text: "Una Vez Aceptado no podra recuperar su receta!",
@@ -12,40 +16,37 @@ const ItemRecipes = ({receta,funcionSetReceta}) => {
       confirmButtonColor: "#3085d6",
       cancelButtonColor: "#d33",
       confirmButtonText: "Borrar",
-      cancelButtonText: "Cancelar"
-    }).then( async (result) => {
+      cancelButtonText: "Cancelar",
+    }).then(async result => {
       if (result.isConfirmed) {
-        const respuesta = await  borrarRecetaAPI(receta.id)
-        if(respuesta.status === 200){
+        const respuesta = await borrarRecetaAPI(receta.id);
+        if (respuesta.status === 200) {
           Swal.fire({
             title: "La Receta ah sido Eliminada ",
             text: `La Receta "${receta.nombreRecetas}" fue eliminada exitosamente`,
-            icon: "success"
-          }); 
-          const respuestaReceta= await mostraRecetaAPI()
-          if(respuestaReceta.status === 200){
+            icon: "success",
+          });
+          const respuestaReceta = await mostraRecetaAPI();
+          if (respuestaReceta.status === 200) {
             const recetaAlmacenadas = await respuestaReceta.json();
-            funcionSetReceta(recetaAlmacenadas)
-          }else{
+            funcionSetReceta(recetaAlmacenadas);
+          } else {
             Swal.fire({
               title: "Upps ocurrio algun error ",
               text: `No se econtraron las Recetas`,
-              icon: "error"
+              icon: "error",
             });
-
           }
-        }else{
+        } else {
           Swal.fire({
             title: "Upps ocurrio algun error ",
             text: `La Receta "${receta.nombreRecetas}" no pudo ser eliminada intentalo en unos minutos`,
-            icon: "error"
+            icon: "error",
           });
         }
-       
       }
     });
-  }
-
+  };
 
   return (
     <tr>
@@ -64,8 +65,16 @@ const ItemRecipes = ({receta,funcionSetReceta}) => {
       <td>{receta.descripcion}</td>
       <td>{receta.instrucciones}</td>
       <td>
-        <Link   className="me-lg-2 btn btn-warning" to={'/admin/edit/'+ receta.id}>editar</Link>
-        <Button variant="danger" onClick={borrarRecetas}> delete</Button>
+        <Link
+          className="me-lg-2 btn btn-warning"
+          to={"/admin/edit/" + receta.id}
+        >
+          editar
+        </Link>
+        <Button variant="danger" onClick={borrarRecetas}>
+          {" "}
+          delete
+        </Button>
       </td>
     </tr>
   );
